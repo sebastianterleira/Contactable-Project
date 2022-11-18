5d6d025aa228e1cb2c5ccf94cac413fd30b9c644
sebastianterleira <seb.terleira1204@gmail.com>
2022-11-18 13:59:36 -0500

    add: sessions-service file

---
+++ index.js
@@ -0,0 +1,39 @@
+import { login, logout } from "./src/services/sessions-service.js";
+
+const crendentials = {
+	email: "seb.terleira1204@mail.com",
+	password: "123456",
+};
+
+async function test() {
+	try {
+		const user = await login(crendentials);
+		console.log(user);
+		
+		const data = await logout(); 
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+test();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// login(crendentials)
+// 	.then((user) => console.log(user))
+// 	.catch((error) => console.log(error));
---
+++ src/services/sessions-service.js
@@ -0,0 +1,50 @@
+// Crear el Login y el Logout
+const BASE_URI = "https://contactable-api.herokuapp.com";
+const tokenKey = "contactable_token";
+
+export async function login(credentials = { email, password }) {
+    const response = await fetch(`${BASE_URI}/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+    });
+
+    if (!response.ok) {
+        const data = await response.json();
+        throw new Error(data.errors);
+    }
+
+    const data = await response.json();
+    sessionStorage.setItem(tokenKey, data.token);
+
+    return data;
+}
+
+export async function logout() {
+	const token = sessionStorage.getItem(tokenKey);
+
+	const response = await fetch(`${BASE_URI}/logout`, {
+			method: "DELETE",
+			headers: {
+				Authorization: `Token token=${token}`,
+				"Content-Type": "application/json",
+			},
+	});
+
+	let data;
+	try {
+		data = await response.json();
+	} catch (error) {
+		data = response.statusText;
+	}
+	
+	if (!response.ok) {
+		throw new Error(data.errors);
+	}
+
+	sessionStorage.removeItem(tokenKey);
+
+	return data;
+}
